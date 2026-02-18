3d52fde387a4de9a858f8429fd94f2cec826416c
Sergio <sergio.torreslanda@gmail.com>
2026-02-18 18:50:47 -0500

    add spec for ThermalState and BatteryLevel

---
+++ src/specs/NativeGrapheneCore.ts
@@ -0,0 +1,12 @@
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  // 1. Get current battery level (0.0 to 1.0)
+  getBatteryLevel(): Promise<number>;
+  
+  // 2. Get thermal state string ("nominal", "fair", "serious", "critical")
+  getThermalState(): Promise<string>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('GrapheneCore');
